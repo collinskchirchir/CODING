6ae1ae79a2e92bb8e5010cab8d1db73109061f3d
collins-tech-dev <collins-tech-dev@users.noreply.github.com>
2021-03-22 22:25:33 +0300

    Comment last line

--- Chapter05/PeopleApp/Program.cs
+++ Chapter05/PeopleApp/Program.cs
@@ -169,7 +169,7 @@ namespace PeopleApp
                     _                                               => 800M
                 };
                 //WriteLine($"Flight Costs {flightCost:C} for {passenger}");
-                Some Random text
+               
 
                 
             }
